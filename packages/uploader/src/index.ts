43a58eef9b8bec57cb0951e3a9fcb6762f2dc93a
moyuderen <moyuderen.git@gmail.com>
2025-07-01 17:07:26 +0800

    chore: Monorepos

--- packages/uploader/src/index.ts
+++ packages/uploader/src/index.ts
@@ -2,6 +2,6 @@ import './styles/index.scss'
 import Uploader from './components/Uploader'
 import UploadIcon from './icons/UploadIcon'
 export { CheckStatus } from '@tinyuploader/sdk'
-export * from './types/'
+export * from './types'
 
 export { Uploader, UploadIcon }
